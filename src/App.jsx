2589503b949bc737b10ca7dabcb4f57bb515a4e0
Mohammed Akhil <166986897+akhil-Mohammedd@users.noreply.github.com>
2024-07-26 04:54:44 +0000

    favicon added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div className=' bg-slate-800 body h-screen w-full flex justify-center items-center'>
-      <h1 className='text-slate-300 text-3xl font-bold'>Project Initialised</h1>
+      <h1 className='text-slate-300 text-3xl font-bold'>LawFirm</h1>
     </div>
   )
 }
