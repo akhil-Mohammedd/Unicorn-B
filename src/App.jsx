358332473419734f5e6ffa3e053fbd969454cf5a
akhil-Mohammedd <akhil.mohammedd@gmail.com>
2024-07-26 09:59:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className=' bg-slate-800 body h-screen w-full flex justify-center items-center'>
+      <h1 className='text-slate-300 text-3xl font-bold'>Project Initialised</h1>
+    </div>
+  )
+}
+
+export default App
